e2cf91d2403a6154d5055376738e04dc4b7fa9e1
Vanese <vanesebuchanan@gmail.com>
2022-10-25 17:58:53 -0400

    added function for timer

--- script.js
+++ script.js
@@ -10,87 +10,61 @@
 // WHEN the game is over
 // THEN I can save my initials and my score
 
+let start = document.querySelector("#start");
+let scores = document.querySelector("#scores");
+let card = document.querySelector("#card");
 
+// Function to create countdown timer
+let timer = 60;
 
-var startButton = document.querySelector("#start");
-var timeEl = document.querySelector("#time");
-var hiddenQuestion1 = document.getElementById("hidden-question-card1");
-var hiddenQuestion2 = document.getElementById("hidden-question-card2");
-var hiddenQuestion3 = document.getElementById("hidden-question-card3");
-var hiddenQuestion4 = document.getElementById("hidden-question-card4");
-var hiddenQuestion5 = document.getElementById("hidden-question-card5");
-var hiddenStartEl = document.getElementById("hidden-question");
-var questionEl = [hiddenQuestion1, hiddenQuestion2, hiddenQuestion3, hiddenQuestion4, hiddenQuestion5];
-var questionIndex = 0;
-var quizRunning = false;
-var quizContainer = document.getElementById("quiz-container")
-
-
-function hidden() {
-    hiddenQuestion1.style.display = "none";
-    hiddenQuestion2.style.display = "none";
-    hiddenQuestion3.style.display = "none";
-    hiddenQuestion4.style.display = "none";
-    hiddenQuestion5.style.display = "none";
-
+function countdown() {
+  timer--;
+  document.getElementById("seconds").innerHTML = String(timer);
+  if (timer > 0) {
+    setTimeout(countdown, 1000);
+  }
 }
+
+setTimeout(countdown, 1000);
+
+console.log(countdown);
 
 // Quiz questions, options and correct answer
-var questions = [
-    {
-        question: "Javascript is known as what type of language?",
-        options: [
-            "a: Coffee",
-            "b: Cursive",
-            "c: Scripting"],
-            answer: "c: Scripting",
-        },
-    {
-        question: "Javascript is known for development of?",
-        options: [
-            "a: web pages",
-            "b: coffee beans",
-            "c: functions"],
-            answer: "a: web pages",
-        },
-    {
-        question: "True or False. Javascript can be embedded in HTML",
-        options: [
-            "a: True",
-            "b: False"],
-            answer:  "a: Yes", 
-        },  
-    {
-        question: "Javascript cannot be used for which of these?",
-        options: [
-            "a: websites",
-            "b: web servers",
-            "c: reading and writing of files on client side"],
-            answer: "c: reading and writing of files on client side",
-        },
-    {
-        question: "Which of these is part of the Javascript library?",
-        options: [
-            "a: functions",
-            "b: aces",
-            "c: kings"],
-            answer: "b",
-        },  
-    
-];
-function startQuiz() {
-   var questionText = document.createElement("p")
-   questionText.textContent = questions[questionIndex].question
-    quizContainer.appendChild(questionText)
-}
+// const questionArray = [
+//   {
+//     question: "Javascript is known as what type of language?",
+//     options: ["a: Coffee", "b: Cursive", "c: Scripting"],
+//     answer: "c: Scripting",
+//   },
+//   {
+//     question: "Javascript is known for development of?",
+//     options: ["a: web pages", "b: coffee beans", "c: functions"],
+//     answer: "a: web pages",
+//   },
+//   {
+//     question: "True or False. Javascript can be embedded in HTML",
+//     options: ["a: True", "b: False"],
+//     answer: "a: True",
+//   },
+//   {
+//     question: "Javascript cannot be used for which of these?",
+//     options: [
+//       "a: websites",
+//       "b: web servers",
+//       "c: reading and writing of files on client side",
+//     ],
+//     answer: "c: reading and writing of files on client side",
+//   },
+//   {
+//     question: "Which of these is part of the Javascript library?",
+//     options: ["a: functions", "b: aces", "c: kings"],
+//     answer: "b",
+//   },
+// ];
 
-function checkAnswer() {
+// function checkAnswer() {}
 
-}
+// checkAnswer();
 
-checkAnswer();
-
-
-// Event listener
-startButton.addEventListener("click",startQuiz)
-    
+// // Event listener
+// startButton.addEventListener("click", startQuiz);
