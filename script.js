9b5b07a880b2cf48a14780fc41114eb226efa015
Vanese <vanesebuchanan@gmail.com>
2022-10-27 10:56:00 -0400

    seperate question file from main js

--- script.js
+++ script.js
@@ -29,38 +29,7 @@ setTimeout(countdown, 1000);
 
 console.log(countdown);
 
-// Quiz questions, options and correct answer
-// const questionArray = [
-//   {
-//     question: "Javascript is known as what type of language?",
-//     options: ["a: Coffee", "b: Cursive", "c: Scripting"],
-//     answer: "c: Scripting",
-//   },
-//   {
-//     question: "Javascript is known for development of?",
-//     options: ["a: web pages", "b: coffee beans", "c: functions"],
-//     answer: "a: web pages",
-//   },
-//   {
-//     question: "True or False. Javascript can be embedded in HTML",
-//     options: ["a: True", "b: False"],
-//     answer: "a: True",
-//   },
-//   {
-//     question: "Javascript cannot be used for which of these?",
-//     options: [
-//       "a: websites",
-//       "b: web servers",
-//       "c: reading and writing of files on client side",
-//     ],
-//     answer: "c: reading and writing of files on client side",
-//   },
-//   {
-//     question: "Which of these is part of the Javascript library?",
-//     options: ["a: functions", "b: aces", "c: kings"],
-//     answer: "b",
-//   },
-// ];
+
 
 // function checkAnswer() {}
 
